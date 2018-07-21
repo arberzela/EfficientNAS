bbc08cbc33912ef533f96b9c6c1c3eb7325b2b14
Arber Zela <arber.zela21@gmail.com>
2018-07-21 19:58:24 +0200

    Update BOHB-CIFAR10.sh

--- BOHB-CIFAR10.sh
+++ BOHB-CIFAR10.sh
@@ -13,4 +13,4 @@
 #
 #
 source activate pytorch
-python cifar10_master.py --array_id $SLURM_ARRAY_TASK_ID --total_num_workers 20 --num_iterations 64 --run_id $SLURM_ARRAY_JOB_ID --working_directory ./data/run_$SLURM_ARRAY_JOB_ID
+python cifar10_master.py --array_id $SLURM_ARRAY_TASK_ID --total_num_workers 10 --num_iterations 32 --run_id $SLURM_ARRAY_JOB_ID --working_directory ./data/run_$SLURM_ARRAY_JOB_ID
